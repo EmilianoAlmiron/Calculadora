aba204203c61968a83cb76b4aee1f3c1ce73f361
Cordobez <81971013+EmilianoAlmiron@users.noreply.github.com>
2024-06-29 17:43:22 -0300

    fin

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ let pantalla  = document.getElementById("pantalla");
 let igual = document.getElementById("igual");
 let teclas = document.getElementsByClassName("tecla");
 let operaciones = document.getElementsByClassName("operacion");
+let borrar1 = document.getElementById("borrar1")
+let borrarT = document.getElementById("borrarT")
 let operacion = "";
 let operando1 = "";
 let operando2 = "";
@@ -23,27 +25,23 @@ for (let i = 0; i < operaciones.length; i++) {
         switch (operaciones[i].innerText) {
             case "+":
                 operacion= "suma";
+                pantalla.innerText = "+";
                 break;
             case "-":
                 operacion = "resta";
+                pantalla.innerText = "-";
                 break;
             case "*":
                 operacion = "multiplicacion";
+                pantalla.innerText = "*";
                 break;
             case "/":
                 operacion = "division";
-                break;
-            case "%":
-                operacion = "porcentaje";
-                break;
-            case "**":
-                operacion = "elevadocuadrado";
+                pantalla.innerText = "/";
                 break;
             default:
                 console.log("Operacion no valida");
         }
-        pantalla.innerText = "";
-        console.log(operaciones);
     })
 }
 
@@ -53,23 +51,49 @@ igual.addEventListener("click", () =>{
     switch (operacion) {
         case "suma":
             pantalla.innerText = operando1 + operando2;
+            operando1 = parseFloat(pantalla.innerText);
+            operando2 = "";
             break;
         case "resta":
             pantalla.innerText = operando1 - operando2;
-        break;
+            operando1 = parseFloat(pantalla.innerText);
+            operando2 = "";
+            break;
         case "multiplicacion":
             pantalla.innerText = operando1 * operando2;
-        break;
+            operando1 = parseFloat(pantalla.innerText);
+            operando2 = "";
+            break;
         case "division":
-            pantalla.innerText = operando1 / operando2;
-        break;
-        case "porcentaje":
-            pantalla.innerText = operando1 % operando2;
-        break;
-        case "elevadocuadrado":
-            pantalla.innerText = operando1 * operando1;
-        break;
+            if(operando2 <= 0){
+                console.log("No se puede realizar la operacion");
+                pantalla.innerText = "";
+                break;
+            }else{
+                pantalla.innerText = operando1 / operando2;
+                operando1 = parseFloat(pantalla.innerText);
+                operando2 = "";
+                break;
+            }
         default:
-            console.log("operacion no valida");;
+            console.log("operacion no valida");
+            pantalla.innerText = "";
     }
+})
+
+borrar1.addEventListener("click", () =>{
+    if (operacion === ""){
+        operando1 = operando1.slice(0, operando1.length -1)
+        pantalla.innerText = operando1;
+    }else{
+        operando2 = operando2.slice(0, operando2.length -1)
+        pantalla.innerText = operando2;
+    }
+})
+
+borrarT.addEventListener("click", () =>{
+    pantalla.innerText= "";
+    operando1= "";
+    operando2="";
+    operacion="";
 })
